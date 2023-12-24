d79302c94d4ffbc65f2fb616aef0d08d98a099a9
dmitryraven20 <123392975+dmitryraven20@users.noreply.github.com>
2023-12-24 23:33:21 +0800

    Update find_vid.py

--- find_vid.py
+++ find_vid.py
@@ -2,11 +2,11 @@ import cv2
 
 cap = cv2.VideoCapture("output.avi")
 img = cv2.imread("img.png")
+length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
 counter = 0
-
                 
-while cap.isOpened():
+while counter != length/100:
     _, frame = cap.read()
     
     img = cv2.resize(img, (640, 480), interpolation=cv2.INTER_AREA)
@@ -17,10 +17,10 @@ while cap.isOpened():
     _, threshvid = cv2.threshold(grayvid,0,255,cv2.THRESH_BINARY)
     _, threshimg = cv2.threshold(grayimg,0,255,cv2.THRESH_BINARY)
 
-    compare = cv2.matchTemplate(threshvid,threshimg,cv2.TM_CCOEFF)
+    compare = cv2.matchTemplate(threshvid,threshimg,cv2.TM_CCOEFF_NORMED)
+
     if compare > 0.9:
         counter += 1
-        print("Совпадение есть")
+        # print("Совпадение есть")
 
-cap.release
 print(f"Итого {counter}")
